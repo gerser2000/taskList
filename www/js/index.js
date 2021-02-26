d1168ce9a10848982c0094e913cb063a50fed053
gerser2000 <nomengerard@gmail.com>
2021-02-26 16:49:54 +0100

    add1

--- www/js/index.js
+++ www/js/index.js
@@ -3,10 +3,20 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
+var add_button = document.getElementById("log_button");
+
+var list = document.getElementById("taskList");
+function afegeix_tasca(){
+	var ntask = document.getElementById("ntask").value;
+	console.log("afegint tasca");
+	$('#listview').append
+
+}
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+	document.getElementById("add_button").onclick = afegeix_tasca;
 }
